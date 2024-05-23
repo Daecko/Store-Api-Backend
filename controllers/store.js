82ed50edff9e8bf39cf8c3374610d0943c829407
Daecko <properlydone485@gmail.com>
2024-05-22 21:43:13 -0600

    trying to fix(dir:sqlite) 5:config

--- controllers/store.js
+++ controllers/store.js
@@ -1,7 +1,7 @@
 import { getCollections } from "../sqlite/db.js";
 import { errorResponse, successResponse } from "../utility/utils.js";
 
-export const retrieveCollections = async (req, res) => {
+export const retrieveCollections = async (req, res, next) => {
   try {
     const data = await getCollections();
     return successResponse(res, data);
